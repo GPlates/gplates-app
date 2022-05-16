255c9c59443420ad2e3e8921e9151830e2dcd90f
yiyanw <31875861+yiyanw@users.noreply.github.com>
2022-05-16 12:56:11 +1000

    fixed the bug that layout crashed in IOS platform (#19)

--- src/pages/SettingMenuPage.tsx
+++ src/pages/SettingMenuPage.tsx
@@ -1,4 +1,5 @@
 import React, { useState } from 'react'
+import './SettingMenuPage.css'
 import {
   IonModal,
   IonButton,
@@ -7,8 +8,13 @@ import {
   IonTitle,
   IonRippleEffect,
   IonRange,
-  IonContent, IonToggle, IonCheckbox, IonSelectOption, IonSegment, IonSegmentButton, IonLabel,
-  IonSelect
+  IonToggle,
+  IonCheckbox,
+  IonSelectOption,
+  IonSegment,
+  IonSegmentButton,
+  IonLabel,
+  IonSelect, IonContent, IonList, IonItemDivider, IonItem, IonRadioGroup, IonRadio, IonButtons
 } from '@ionic/react'
 
 interface ContainerProps {
@@ -23,64 +29,73 @@ export const SettingMenuPage: React.FC<ContainerProps> = ({ isShow, closeModal }
               animated
               backdropDismiss={false}
     >
-      <IonHeader>
-        <IonToolbar color={'primary'}>
-          <div style={{ display: 'flex', flexDirection: 'row' }}>
-            <IonTitle>Setting Menu</IonTitle>
-            <IonButton
-              onClick={() => {
-                closeModal()
-              }}
-              color={'secondary'}
-            >
-              Close
-              <IonRippleEffect />
-            </IonButton>
-          </div>
-        </IonToolbar>
 
-        {/* put new settings in this div element below */}
-        {/* some demo is shown below */}
-        <div style={{ margin: '1rem 0rem 1rem 0rem' }}>
-          <IonToolbar>
-            <div style={{ marginTop: '0.5rem' }}>
-              <IonTitle>Animation Speed</IonTitle>
-              <IonRange min={20} max={80} step={2} />
-            </div>
-            <div style={{ display: 'flex', flexDirection: 'row', marginTop: '0.5rem' }}>
-              <IonTitle style={{ marginTop: '0.5rem' }}>Background Color</IonTitle>
-              <IonToggle />
-            </div>
-            <div style={{ display: 'flex', flexDirection: 'row', marginRight: '0.7rem', marginTop: '0.5rem' }}>
-              <IonTitle style={{ marginTop: '0.5rem' }}>Enable Something</IonTitle>
-              <IonCheckbox />
-            </div>
-            <div style={{ display: 'flex', flexDirection: 'row', marginRight: '0.7rem', marginTop: '0.5rem' }}>
-              <IonTitle style={{ marginTop: '0.5rem' }}>Some Segmentation</IonTitle>
-              <IonSegment value='sunny'>
-                <IonSegmentButton value='T1'>
-                  <IonLabel>Sunny</IonLabel>
-                </IonSegmentButton>
-                <IonSegmentButton value='T2'>
-                  <IonLabel>Rainy</IonLabel>
-                </IonSegmentButton>
-              </IonSegment>
-            </div>
-            <div style={{ display: 'flex', flexDirection: 'row', marginRight: '0.7rem', marginTop: '0.5rem' }}>
-              <IonTitle style={{ marginTop: '0.5rem' }}>Select Something</IonTitle>
-              <IonSelect>
-                <IonSelectOption value='1'>1</IonSelectOption>
-                <IonSelectOption value='2'>2</IonSelectOption>
-                <IonSelectOption value='3'>3</IonSelectOption>
-                <IonSelectOption value='4'>4</IonSelectOption>
-              </IonSelect>
-            </div>
+      <IonToolbar>
+        <IonTitle>Setting Menu</IonTitle>
+        <IonButtons slot={'end'}>
+          <IonButton
+            onClick={() => {
+              closeModal()
+            }}
+            color={'secondary'}
+          >
+            Close
+            <IonRippleEffect />
+          </IonButton>
+        </IonButtons>
+      </IonToolbar>
 
-          </IonToolbar>
-        </div>
+      {/* put new settings in this IonList element below */}
+      {/* some demo is shown below */}
+      <IonList>
+        <IonItemDivider>Main Setting Section1</IonItemDivider>
+        <IonItem>
+          <IonLabel>Animation Speed</IonLabel>
+        </IonItem>
+        <IonItem>
+          <IonRange min={20} max={80} step={2} />
+        </IonItem>
 
-        {/*</div>*/}
-      </IonHeader>
+        <IonItemDivider>Main Setting Section2</IonItemDivider>
+        <IonItem>
+          <IonLabel>Background Color</IonLabel>
+          <IonToggle />
+        </IonItem>
+
+        <IonItemDivider>Main Setting Section3</IonItemDivider>
+        <IonItem>
+          <IonLabel>Enable Something</IonLabel>
+          <IonCheckbox class={'single-setting-option'} />
+        </IonItem>
+
+        <IonItemDivider>Main Setting Section4</IonItemDivider>
+        <IonItem>
+          <IonLabel>Some Segmentation</IonLabel>
+          <IonRadioGroup>
+            <IonItem>
+              <IonItem>
+                <IonLabel>1</IonLabel>
+                <IonRadio slot='end' value='1' />
+              </IonItem>
+              <IonItem>
+                <IonLabel>2</IonLabel>
+                <IonRadio slot='end' value='2' />
+              </IonItem>
+            </IonItem>
+          </IonRadioGroup>
+        </IonItem>
+
+        <IonItemDivider>Main Setting Section5</IonItemDivider>
+        <IonItem>
+          <IonLabel>Select Something</IonLabel>
+          <IonSelect>
+            <IonSelectOption value='1'>1</IonSelectOption>
+            <IonSelectOption value='2'>2</IonSelectOption>
+            <IonSelectOption value='3'>3</IonSelectOption>
+            <IonSelectOption value='4'>4</IonSelectOption>
+          </IonSelect>
+        </IonItem>
+      </IonList>
     </IonModal>
   )
 
