06e752f9d98647dd407c8c7b678476e39031b0d5
Matthew Merkas <101497645+matthewmerkas@users.noreply.github.com>
2022-07-17 09:58:33 +1000

    Time dependent rasters (#74)
    * Move Animation Settings into a sub-menu
    
    * Added image cache for SingleTileImageryProvider
    
    * Open age slider on age click. Resolves #65
    
    * Shift icons down on iPhones with notches. Increase spacing between buttons. Resolves #62; resolves #63
    
    * Fixed settings menu safe area
    
    * Fixed settings list top offset
    
    * Replace Sass variables with CSS variables in CSS functions
    
    * Initial time-dependent rasters commit
    
    * Increased number of layers to eliminate flickering at higher FPS
    
    * Range slider tweaks
    
    * Updated build files

--- src/components/AgeSlider.tsx
+++ src/components/AgeSlider.tsx
@@ -17,26 +17,32 @@ import {
   playForwardOutline,
   timeOutline,
 } from 'ionicons/icons'
-import { Dispatch, SetStateAction, useState } from 'react'
+import React, { Dispatch, SetStateAction, useState } from 'react'
 import { setNumber } from '../functions/input'
+import { AnimationService } from '../functions/animation'
 
 interface AgeSliderProps {
   buttons: any
-  age: number
-  setAge: Dispatch<SetStateAction<number>>
+  animationService: AnimationService
+  minAge: number
+  maxAge: number
   setMenuPath: Dispatch<SetStateAction<string>>
   setMenuState: Dispatch<SetStateAction<boolean>>
 }
 
-const AgeSlider = (props: AgeSliderProps) => {
+const AgeSlider: React.FC<AgeSliderProps> = ({
+  buttons,
+  animationService,
+  minAge,
+  maxAge,
+  setMenuPath,
+  setMenuState,
+}) => {
   const [hidden, setHidden] = useState(true)
-  const [playing, setPlaying] = useState(false)
-  const minAge = 0
-  const maxAge = 1000
 
   const openMenu = () => {
-    props.setMenuPath('animation')
-    props.setMenuState(true)
+    setMenuPath('animation')
+    setMenuState(true)
   }
 
   return (
@@ -49,9 +55,9 @@ const AgeSlider = (props: AgeSliderProps) => {
             min={minAge}
             max={maxAge}
             onIonChange={(e) =>
-              setNumber(props.setAge, e.detail.value, minAge, maxAge)
+              setNumber(animationService.setAge, e.detail.value, minAge, maxAge)
             }
-            value={props.age}
+            value={animationService.age}
           />
           Ma
         </IonItem>
@@ -59,18 +65,34 @@ const AgeSlider = (props: AgeSliderProps) => {
           <div>
             <IonButton
               fill="clear"
-              onClick={() => setPlaying(!playing)}
+              onClick={() =>
+                animationService.setPlaying(!animationService.playing)
+              }
               size="default"
             >
-              <IonIcon icon={playing ? pauseOutline : playOutline} />
+              <IonIcon
+                icon={animationService.playing ? pauseOutline : playOutline}
+              />
             </IonButton>
-            <IonButton fill="clear" size="default">
+            <IonButton
+              fill="clear"
+              onClick={() => animationService.resetPlayHead()}
+              size="default"
+            >
               <IonIcon icon={playSkipBackOutline} />
             </IonButton>
-            <IonButton fill="clear" size="default">
+            <IonButton
+              fill="clear"
+              onClick={() => animationService.movePlayHead(-1)}
+              size="default"
+            >
               <IonIcon icon={playBackOutline} />
             </IonButton>
-            <IonButton fill="clear" size="default">
+            <IonButton
+              fill="clear"
+              onClick={() => animationService.movePlayHead(1)}
+              size="default"
+            >
               <IonIcon icon={playForwardOutline} />
             </IonButton>
             <IonButton fill="clear" onClick={openMenu} size="default">
@@ -81,10 +103,20 @@ const AgeSlider = (props: AgeSliderProps) => {
         <IonItem className="slider" lines="none">
           <IonRange
             dir="rtl"
+            onIonKnobMoveStart={() => {
+              animationService.setDragging(true)
+              animationService.setPlaying(false)
+            }}
+            onIonKnobMoveEnd={(e) => {
+              animationService.setDragging(false)
+              animationService.onAgeSliderChange(e.detail.value as number)
+            }}
             min={minAge}
             max={maxAge}
-            onIonChange={(e) => props.setAge(e.detail.value as number)}
-            value={props.age}
+            onIonChange={(e) =>
+              animationService.setAge(e.detail.value as number)
+            }
+            value={animationService.age}
           />
         </IonItem>
       </div>
@@ -97,10 +129,10 @@ const AgeSlider = (props: AgeSliderProps) => {
             setHidden(!hidden)
           }}
         >
-          {props.age} Ma
+          {animationService.age} Ma
         </div>
         <div>
-          {props.buttons}
+          {buttons}
           <IonButton
             className="round-button show-button"
             onClick={() => {
