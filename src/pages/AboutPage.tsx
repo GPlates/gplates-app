19781ad9e99237bf1392aa3ab09de82222dbe279
michael <michael.chin@sydney.edu.au>
2022-07-22 17:41:42 +1000

    Add copyright info
    use <a> tag with links
    
    use Prettier to format AboutPage.tsx
    
    align text to left

--- src/pages/AboutPage.tsx
+++ src/pages/AboutPage.tsx
@@ -6,13 +6,13 @@ import {
   IonModal,
   IonRippleEffect,
   IonTitle,
-  IonToolbar
+  IonToolbar,
 } from '@ionic/react'
 
 import './AboutPage.scss'
 import { Browser } from '@capacitor/browser'
 
-import { Share } from '@capacitor/share';
+import { Share } from '@capacitor/share'
 interface ContainerProps {
   isShow: boolean
   closeModal: Function
@@ -25,8 +25,7 @@ const sentEmail = async (email: string) => {
   })
 }
 
-export const AboutPage: React.FC<ContainerProps> = ({ isShow,
-                                                      closeModal}) => {
+export const AboutPage: React.FC<ContainerProps> = ({ isShow, closeModal }) => {
   return (
     <IonModal isOpen={isShow} animated backdropDismiss={false}>
       <IonToolbar>
@@ -45,27 +44,80 @@ export const AboutPage: React.FC<ContainerProps> = ({ isShow,
       </IonToolbar>
       <IonContent>
         <div className={'about-page-icon'}>
-          <img src={"assets/icon/icon.png"}  alt={'app icon'} />
+          <img src={'assets/icon/icon.png'} alt={'app icon'} />
         </div>
 
         <div className={'about-page-intro-frame'}>
           <p className={'about-page-intro-text'}>
-            The GPlates App is an education and outreach focussed application allowing users to reconstruct and visualise spatial data through geological time.
-            It is a product of the plate reconstruction software, GPlates
-            <IonButton className={'about-page-clickable-info'} color={'tertiary'} onClick={async () => { await Browser.open({ url: "https://www.gplates.org" })}}>https://www.gplates.org</IonButton>
-            and the GPlates Portal
-            <IonButton color={'tertiary'} className={'about-page-clickable-info'} onClick={async () => { await Browser.open({ url: "https://portal.gplates.org" })}}>https://portal.gplates.org</IonButton> .
+            The GPlates App is an education and outreach focussed application
+            allowing users to reconstruct and visualise spatial data through
+            geological time. It is a product of the plate reconstruction
+            software,&nbsp;
+            <a
+              onClick={async () => {
+                await Browser.open({ url: 'https://www.gplates.org' })
+              }}
+            >
+              GPlates
+            </a>
+            &nbsp;and the&nbsp;
+            <a
+              onClick={async () => {
+                await Browser.open({ url: 'https://portal.gplates.org' })
+              }}
+            >
+              GPlates Portal
+            </a>
+            .
+          </p>
+
+          <p className={'about-page-intro-text'}>
+            The GPlates App has been developed at
+            <a
+              onClick={async () => {
+                await Browser.open({ url: 'https://www.sydney.edu.au/' })
+              }}
+            >
+              &nbsp;the University of Sydney&nbsp;
+            </a>
+            and is supported by the Australian government funded
+            <a
+              onClick={async () => {
+                await Browser.open({ url: 'https://www.auscope.org.au/' })
+              }}
+            >
+              &nbsp;AuScope&nbsp;
+            </a>
+            National Collaborative Research Infrastructure System (NCRIS)
+            program.
           </p>
           <p className={'about-page-intro-text'}>
-          The GPlates App has been developed at the University of Sydney and is supported by the Australian government funded AuScope National Collaborative Research Infrastructure System (NCRIS) program.
+            For more information on the GPlate App, please contact
           </p>
-          <p className={'about-page-intro-text'}>For more information on the GPlate App, please contact</p>
-          <p>Maria Seton:
-            <IonButton className={'about-page-clickable-info'} color={'tertiary'} onClick={async () => { await sentEmail('maria.seton@sydney.edu.au')}}>maria.seton@sydney.edu.au</IonButton>
+
+          <p>
+            Maria Seton:&nbsp;
+            <a href="mailto:maria.seton@sydney.edu.au">
+              maria.seton@sydney.edu.au
+            </a>
           </p>
-          <p>Michael Chin:
-            <IonButton className={'about-page-clickable-info'} color={'tertiary'} onClick={async () => { await sentEmail('michael.chin@sydney.edu.au')}}>michael.chin@sydney.edu.au</IonButton>
+          <p>
+            Michael Chin:&nbsp;
+            <a href="mailto:michael.chin@sydney.edu.au">
+              michael.chin@sydney.edu.au
+            </a>
           </p>
+        </div>
+        <div
+          style={{
+            position: 'absolute',
+            bottom: 0,
+            right: 0,
+            fontStyle: 'italic',
+            fontSize: 'small',
+          }}
+        >
+          Copyright © 2022 The University of Sydney. All rights reserved.
         </div>
       </IonContent>
     </IonModal>
