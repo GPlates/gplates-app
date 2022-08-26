58e72e49b8276e71d797aa5458263c0a68c27fa6
michael <michael.chin@sydney.edu.au>
2022-08-26 12:18:49 +1000

    insert and remove points on globe

--- src/components/AddLocationWidget.tsx
+++ src/components/AddLocationWidget.tsx
@@ -13,8 +13,21 @@ import {
   IonInput,
   IonList,
   IonItemDivider,
+  IonModal,
+  IonHeader,
+  IonToolbar,
+  IonButtons,
+  IonTitle,
+  IonContent,
 } from '@ionic/react'
-import { Color, Cartesian2, Entity, Cartographic, Math } from 'cesium'
+import {
+  Color,
+  Cartesian2,
+  Entity,
+  Cartographic,
+  Math,
+  Cartesian3,
+} from 'cesium'
 import './AddLocationWidget.scss'
 import { cesiumViewer } from '../pages/Main'
 
@@ -22,8 +35,8 @@ let cameraChangedRemoveCallback: any = null
 let cameraMoveStartRemoveCallback: any = null
 let cameraMoveEndtRemoveCallback: any = null
 
-var pointerEntity: Entity | null = null
-
+var locationEntities: Entity[] = []
+var locationCartesian: Cartesian3 | undefined = Cartesian3.fromDegrees(0, 0)
 //
 //
 interface AddLocationWidgetProps {
@@ -39,6 +52,8 @@ const AddLocationWidget: React.FC<AddLocationWidgetProps> = ({
 }) => {
   const [lonLat, setLonLat] = useState([0, 0])
   const [lonLatList, setLonLatlist] = useState<[number, number][]>([])
+  const [showLocationDetails, setShowLocationDetails] = useState(false)
+  const [showLocationIndex, setShowLocationIndex] = useState(0)
 
   const cameraHandler = () => {
     let ray = cesiumViewer.scene.camera.getPickRay(
@@ -49,23 +64,11 @@ const AddLocationWidget: React.FC<AddLocationWidgetProps> = ({
     )
 
     if (ray != undefined) {
-      var p = cesiumViewer.scene.globe.pick(ray, cesiumViewer.scene)
-      if (p) {
-        let pc = Cartographic.fromCartesian(p)
+      locationCartesian = cesiumViewer.scene.globe.pick(ray, cesiumViewer.scene)
+      if (locationCartesian) {
+        let pc = Cartographic.fromCartesian(locationCartesian)
         setLonLat([Math.toDegrees(pc.longitude), Math.toDegrees(pc.latitude)])
       }
-      if (pointerEntity) cesiumViewer.entities.remove(pointerEntity)
-      pointerEntity = cesiumViewer.entities.add({
-        name: 'center',
-        position: p,
-        point: {
-          color: Color.BLACK,
-          pixelSize: 10,
-          outlineColor: Color.YELLOW,
-          outlineWidth: 3,
-          disableDepthTestDistance: Number.POSITIVE_INFINITY,
-        },
-      })
     }
   }
 
@@ -80,7 +83,6 @@ const AddLocationWidget: React.FC<AddLocationWidgetProps> = ({
     if (cameraChangedRemoveCallback) cameraChangedRemoveCallback()
     if (cameraMoveStartRemoveCallback) cameraMoveStartRemoveCallback()
     if (cameraMoveEndtRemoveCallback) cameraMoveEndtRemoveCallback()
-    if (pointerEntity) cesiumViewer.entities.remove(pointerEntity)
   }
   return (
     <div>
@@ -91,63 +93,125 @@ const AddLocationWidget: React.FC<AddLocationWidgetProps> = ({
       <div
         className={show ? 'add-locate-widget show' : 'add-locate-widget hide'}
       >
-        <div>
-          <IonList className="location-list">
-            <IonItemDivider>Location List</IonItemDivider>
-            {lonLatList.map((location, index) => {
-              return (
-                <IonItem key={index}>
-                  <IonLabel>Longitude:</IonLabel>
-                  <IonInput readonly value={location[0]}></IonInput>
+        <IonList className="location-list">
+          <IonItemDivider>Location List</IonItemDivider>
+          {lonLatList.map((location, index) => {
+            return (
+              <IonItem key={index}>
+                <IonLabel>Longitude:</IonLabel>
+                <IonInput readonly value={location[0]}></IonInput>
 
-                  <IonLabel>Latitude:</IonLabel>
-                  <IonInput readonly value={location[1]}></IonInput>
-                  <IonButton color="tertiary">
-                    <IonIcon icon={informationOutline} />
-                  </IonButton>
-                  <IonButton
-                    color="tertiary"
-                    onClick={() => {
-                      let a = [...lonLatList]
-                      a.splice(index, 1)
-                      setLonLatlist(a)
-                    }}
-                  >
-                    <IonIcon icon={trashOutline} />
-                  </IonButton>
-                </IonItem>
-              )
-            })}
-          </IonList>
-          <IonItemDivider>Insert Location</IonItemDivider>
+                <IonLabel>Latitude:</IonLabel>
+                <IonInput readonly value={location[1]}></IonInput>
+                <IonButton
+                  color="tertiary"
+                  onClick={() => {
+                    setShowLocationIndex(index)
+                    setShowLocationDetails(true)
+                  }}
+                >
+                  <IonIcon icon={informationOutline} />
+                </IonButton>
+                <IonButton
+                  color="tertiary"
+                  onClick={() => {
+                    let a = [...lonLatList]
+                    a.splice(index, 1)
+                    setLonLatlist(a)
+                    cesiumViewer.entities.remove(locationEntities[index])
+                    locationEntities.splice(index, 1)
+                  }}
+                >
+                  <IonIcon icon={trashOutline} />
+                </IonButton>
+              </IonItem>
+            )
+          })}
+        </IonList>
+        <IonItemDivider>Insert Location</IonItemDivider>
+        <IonItem>
+          <IonLabel>Longitude:</IonLabel>
+          <IonInput type="number" value={lonLat[0]}></IonInput>
+
+          <IonLabel>Latitude:</IonLabel>
+          <IonInput type="number" value={lonLat[1]}></IonInput>
+          <IonButton
+            id="open-modal"
+            color="primary"
+            onClick={() => {
+              setLonLatlist(lonLatList.concat([[lonLat[0], lonLat[1]]]))
+              let pe = cesiumViewer.entities.add({
+                name:
+                  'Index(' +
+                  String(lonLatList.length) +
+                  ') Lon: ' +
+                  String(lonLat[0]) +
+                  ' Lat: ' +
+                  String(lonLat[1]),
+                position: locationCartesian,
+                point: {
+                  color: Color.BLACK,
+                  pixelSize: 10,
+                  outlineColor: Color.YELLOW,
+                  outlineWidth: 3,
+                  disableDepthTestDistance: Number.POSITIVE_INFINITY,
+                },
+              })
+              locationEntities.push(pe)
+            }}
+          >
+            Insert
+          </IonButton>
+        </IonItem>
+
+        <IonIcon
+          icon={closeOutline}
+          className="close-button"
+          slot={'end'}
+          color="secondary"
+          size="small"
+          onClick={() => {
+            setShow(false)
+          }}
+        ></IonIcon>
+      </div>
+
+      <IonModal isOpen={showLocationDetails} animated backdropDismiss={false}>
+        <IonHeader>
+          <IonToolbar>
+            <IonTitle>Location Details</IonTitle>
+            <IonButtons slot="end">
+              <IonButton onClick={() => setShowLocationDetails(false)}>
+                Close
+              </IonButton>
+            </IonButtons>
+          </IonToolbar>
+        </IonHeader>
+        <IonContent>
           <IonItem>
             <IonLabel>Longitude:</IonLabel>
-            <IonInput type="number" value={lonLat[0]}></IonInput>
+            <IonInput
+              readonly
+              value={
+                lonLatList.length > showLocationIndex
+                  ? lonLatList[showLocationIndex][0]
+                  : 0
+              }
+            ></IonInput>
 
             <IonLabel>Latitude:</IonLabel>
-            <IonInput type="number" value={lonLat[1]}></IonInput>
-            <IonButton
-              color="primary"
-              onClick={() => {
-                setLonLatlist(lonLatList.concat([[lonLat[0], lonLat[1]]]))
-              }}
-            >
-              Insert
-            </IonButton>
+            <IonInput
+              readonly
+              value={
+                lonLatList.length > showLocationIndex
+                  ? lonLatList[showLocationIndex][1]
+                  : 0
+              }
+            ></IonInput>
           </IonItem>
-
-          <IonIcon
-            icon={closeOutline}
-            className="close-button"
-            slot={'end'}
-            color="secondary"
-            size="small"
-            onClick={() => {
-              setShow(false)
-            }}
-          ></IonIcon>
-        </div>
-      </div>
+          <p>TODO: collect more info about this location and put here</p>
+        </IonContent>
+      </IonModal>
     </div>
   )
 }
