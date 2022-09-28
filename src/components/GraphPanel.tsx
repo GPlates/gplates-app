a0ea1fac9310370ae844c0ae1f92e59bad961d7c
yiyan wang <13171209963@163.com>
2022-09-28 12:59:33 +1000

    add pop up graph list for graph panel component. limit range of data within valid interval.

--- src/components/GraphPanel.tsx
+++ src/components/GraphPanel.tsx
@@ -1,10 +1,19 @@
 import * as echarts from 'echarts'
 import React, { useEffect, useState } from 'react'
 import { useRecoilState, useRecoilValue } from 'recoil'
-import { age, isGraphPanelShowState } from '../functions/atoms'
+import { age, animateRange, isGraphPanelShowState } from '../functions/atoms'
 import './GraphPanel.scss'
 import { EChartsType } from 'echarts'
-import { getPlatforms } from '@ionic/react'
+import {
+  getPlatforms,
+  IonAccordion,
+  IonAccordionGroup,
+  IonItem,
+  IonLabel,
+  IonList,
+  useIonLoading,
+} from '@ionic/react'
+import rasterMaps from '../functions/rasterMaps'
 
 type EChartsOption = echarts.EChartsOption
 let MY_CHART: EChartsType
@@ -14,10 +23,15 @@ let Y_VALS
 let DATA
 let DATA_MAP: string[]
 
-async function getData() {
-  let data: any = await fetch(
-    'https://gws.gplates.org/mobile/get_scotese_etal_2021_global_temp'
-  )
+// test dummy data
+const urlList = [
+  'https://gws.gplates.org/mobile/get_scotese_etal_2021_global_temp',
+  'https://gws.gplates.org/mobile/get_scotese_etal_2021_global_temp',
+  'https://gws.gplates.org/mobile/get_scotese_etal_2021_global_temp',
+]
+
+async function getData(url: string) {
+  let data: any = await fetch(url)
   let data_map = await data.json()
 
   let x_vals: number[] = []
@@ -40,75 +54,95 @@ interface ContainerProps {}
 export const GraphPanel: React.FC<ContainerProps> = () => {
   const isShow = useRecoilValue(isGraphPanelShowState)
   const [_age, setAge] = useRecoilState(age)
-  // initialization
-  useEffect(() => {
-    getData().then(([data_map, data, x_vals, y_vals]) => {
-      DATA_MAP = data_map
-      DATA = data
-      X_VALS = x_vals
-      Y_VALS = y_vals
+  const rasterMapAnimateRange = useRecoilValue(animateRange)
+  const [curGraphIdx, setCurGraphIdx] = useState(0)
+  const [present, dismiss] = useIonLoading()
 
-      let chartDom = document.getElementById('graphPanel-statistics')!
-      MY_CHART = echarts.init(chartDom)
+  const loadGraph = async () => {
+    await getData(urlList[curGraphIdx]).then(
+      ([data_map, data, x_vals, y_vals]) => {
+        DATA_MAP = data_map
+        DATA = data.slice(
+          rasterMapAnimateRange.lower,
+          rasterMapAnimateRange.upper
+        )
+        X_VALS = x_vals.slice(
+          rasterMapAnimateRange.lower,
+          rasterMapAnimateRange.upper
+        )
+        Y_VALS = y_vals.slice(
+          rasterMapAnimateRange.lower,
+          rasterMapAnimateRange.upper
+        )
 
-      globalThis.addEventListener('resize', function () {
-        MY_CHART.resize()
-      })
+        let chartDom = document.getElementById('graphPanel-statistics')!
+        MY_CHART = echarts.init(chartDom)
 
-      OPTION = {
-        xAxis: {
-          type: 'category',
-          data: x_vals,
-          axisPointer: {
-            show: true,
-            snap: true,
-            value: _age,
-            handle: {
+        globalThis.addEventListener('resize', function () {
+          MY_CHART.resize()
+        })
+
+        OPTION = {
+          xAxis: {
+            type: 'category',
+            data: X_VALS,
+            axisPointer: {
               show: true,
-              size: 20,
-              margin: 35,
-              color: 'white',
-            },
-            label: {
-              show: true,
-              margin: 3,
-              formatter: (params) => {
-                let num_age = _age
-                if (typeof params.value === 'string') {
-                  num_age = parseInt(params.value)
-                  //setAge(num_age)//need to think about this more
-                }
-                return num_age + 'Ma : ' + DATA_MAP[num_age]
+              snap: true,
+              value: _age,
+              handle: {
+                show: true,
+                size: 20,
+                margin: 35,
+                color: 'white',
+              },
+              label: {
+                show: true,
+                margin: 3,
+                formatter: (params) => {
+                  let num_age = _age
+                  if (typeof params.value === 'string') {
+                    num_age = parseInt(params.value)
+                    //setAge(num_age)//need to think about this more
+                  }
+                  return num_age + 'Ma : ' + DATA_MAP[num_age]
+                },
               },
             },
           },
-        },
 
-        yAxis: {
-          type: 'value',
-        },
-        series: [
-          {
-            data: data,
-            type: 'line',
-            symbol: 'none',
+          yAxis: {
+            type: 'value',
           },
-        ],
-      }
-
-      // for pc mode
-      if (getPlatforms().includes('desktop')) {
-        OPTION.tooltip = {
-          triggerOn: 'none',
+          series: [
+            {
+              data: DATA,
+              type: 'line',
+              symbol: 'none',
+            },
+          ],
         }
-      } else {
-        // for touch device mode
-        // @ts-ignore
-        OPTION.xAxis!.axisPointer.handle.icon = 'none'
+
+        // for pc mode
+        if (getPlatforms().includes('desktop')) {
+          OPTION.tooltip = {
+            triggerOn: 'none',
+          }
+        } else {
+          // for touch device mode
+          // @ts-ignore
+          OPTION.xAxis!.axisPointer.handle.icon = 'none'
+        }
+        OPTION && MY_CHART.setOption(OPTION)
       }
-      OPTION && MY_CHART.setOption(OPTION)
+    )
+  }
+  // initialization
+  useEffect(() => {
+    loadGraph().catch((err) => {
+      console.log(err)
     })
-  }, [])
+  }, [rasterMapAnimateRange])
 
   useEffect(() => {
     if (OPTION == undefined) {
@@ -119,9 +153,44 @@ export const GraphPanel: React.FC<ContainerProps> = () => {
     MY_CHART.setOption(OPTION)
   }, [_age])
 
+  let optionList = []
+  for (let i = 0; i < urlList.length; i++) {
+    optionList.push(
+      <IonItem
+        key={i}
+        onClick={async () => {
+          await present({ message: 'Loading...' })
+          setCurGraphIdx(i)
+          await loadGraph()
+          await dismiss()
+        }}
+      >
+        <IonLabel
+          style={
+            i === curGraphIdx
+              ? { fontWeight: 'bolder', color: 'var(--ion-color-primary)' }
+              : {}
+          }
+        >
+          Graph {i}
+        </IonLabel>
+      </IonItem>
+    )
+  }
+
   return (
     <div style={{ visibility: isShow ? 'visible' : 'hidden' }}>
       <div id="graphPanel-statistics" className="graph-panel-statistics" />
+      <IonAccordionGroup className="graph-panel-list">
+        <IonAccordion value="first">
+          <IonItem slot="header">
+            <IonLabel>Graph List</IonLabel>
+          </IonItem>
+          <div slot="content">
+            <IonList>{optionList}</IonList>
+          </div>
+        </IonAccordion>
+      </IonAccordionGroup>
     </div>
   )
 }
